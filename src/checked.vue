3fbb9c0c789494910c2c1f6bfdc0ce47d4ab48f5
jeremiah <lsj970811@yeah.net>
2022-06-20 22:37:30 +0800

    change

---
+++ src/checked.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <form action="">
+      <i v-for="(item,index) in arr" :key="index">
+        <input type="checkbox" :value="arr[index]" v-model="hobby" />
+        {{ arr[index] }}
+      </i>
+    </form>
+
+    <ul>
+      <li v-for="(item,index) in hobby" :key="index">
+        {{ hobby[index] }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      arr: ['科幻', '喜剧', '动漫', '冒险', '科技', '军事', '娱乐', '奇闻'],
+      hobby: []
+    }
+  }
+}
+</script>
+
+<style></style>
